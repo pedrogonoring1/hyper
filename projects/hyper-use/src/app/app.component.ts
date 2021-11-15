b9aeb91b18f8ecd9c3d6caf6a8bb2f5eac67984c
arlamend7 <38164740+arlamend7@users.noreply.github.com>
2021-11-15 18:13:06 -0300

    adiciona um sistema complexo de erros, label e control

--- projects/hyper-use/src/app/app.component.ts
+++ projects/hyper-use/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hyper-use';
 
   public textBadge = 'bagde maneiro';
@@ -19,4 +20,20 @@ export class AppComponent {
 
   public textBadge4 = 'bagde maneiro';
   public typeBadge4 = 'caution';
-}
+
+  formulario: FormGroup
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(6) ,Validators.email]]
+    })
+  }
+  ngOnInit(): void {
+    const control = this.formulario.get('name');
+    control.valueChanges.subscribe(x => {
+      //console.log(control.errors);
+    })
+  }
+  reset() {
+    this.formulario.reset();
+  }
+    }
--- projects/hyper-use/src/app/app.module.ts
+++ projects/hyper-use/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HyperModule } from 'projects/hyper-angular';
+import { HyperModule } from '../../../hyper/src/public-api';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    HyperModule
+    HyperModule.forRoot('inline'),
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
